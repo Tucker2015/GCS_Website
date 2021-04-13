fe987668373067de80dc4ca92a1bfe7f8b8b7982
Tucker2015 <mail@kevtucker.com>
2021-04-13 11:16:46 +0100

    - Initial Commit

--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,8 @@ import Head from 'next/head'
 
 export default function Layout({ children }) {
 
-    const [isOpen, setIsOpen] = useState(false)
-    const openmenu = () => setIsOpen(!isOpen)
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
     return (
         <>
@@ -21,42 +21,24 @@ export default function Layout({ children }) {
                 />
             </Head>
             <header className={styles.header}>
-                <nav className={styles.navbar}>
-                    <div className={styles.navlogo}><img src="./logo.jpg">
-                    </img> Global Cleaning Solutions</div>
-                    <ul className={isOpen === false ? styles.navmenu : styles.navmenu + ' ' + styles.active}>
-                        <Link href='/'>
-                            <li className={styles.navitem}>
-                                <a className={styles.navlink}>Home</a>
-                            </li>
-                        </Link>
-                        <Link href='/services'>
-                            <li className={styles.navitem}>
-                                <a className={styles.navlink}>Services</a>
-                            </li>
-                        </Link>
-                        <Link href='/contact'>
-                            <li className={styles.navitem}>
-                                <a className={styles.navlink}>Contact</a>
-                            </li>
-                        </Link>
-                        <Link href='/our_work'>
-                            <li className={styles.navitem}>
-                                <a className={styles.navlink}>Work</a>
-                            </li>
-                        </Link>
-
-                    </ul>
-                    <button className={isOpen === false ? styles.hamburger : styles.hamburger + ' ' + styles.active}
-                        onClick={openmenu}
-                    >
-                        <span className={styles.bar}></span>
-                        <span className={styles.bar}></span>
-                        <span className={styles.bar}></span>
-                    </button>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top p-3">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">Global Cleaning Solutions</a>
+                        <button className="custom-toggler navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+                            <span className="navbar-toggler-icon" />
+                        </button>
+                        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
+                            <a className="nav-link text-info" href="/contact">Support</a>
+                            <a className="nav-link text-info" href="/login">Login</a>
+                            <a href="/request-demo" className="btn btn-sm btn-info nav-link text-white" >Request demo</a>
+                        </div>
+                    </div>
                 </nav>
             </header>
             <main>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
                 {children}
 
             </main>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Head from "next/head";
 import Layout from '../components/Layout'
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
@@ -6,9 +7,27 @@ import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
 function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  return (
+    <>
+      <Head>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossorigin="anonymous"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+          crossorigin="anonymous"></script>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
