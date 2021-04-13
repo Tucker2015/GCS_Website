ccf6868819ee14fed5c66bb3ee4763de965c4bb2
Tucker2015 <kevtucker@gmail.com>
2021-04-13 20:38:28 +0100

    - Add information to services

--- pages/services.js
+++ pages/services.js
@@ -27,6 +27,18 @@ export default function Service() {
                         <p>• A basic clean entails cleaning tasks that must be completed at least once per week in order to preserve cleanliness, prevent the accumulation of grime and bacteria, avoiding disorganisation, and keep the Home/Business clean and tidy.</p>
                     </div>
                     <div className={styles.card}>
+                        <h3>WHAT DOES A BASIC CLEAN INCLUDE ?</h3>
+                        <p>This list is not exhaustive since each Home or Business has unique furniture, appliances, and other items.  This list aims to help distinguish between a basic and a deep clean.</p>
+                        <br></br>
+                        <p>• All floors vacuumed and / or mopped.</p>
+                        <p>• General 'Tidy-up' - Organizing, removing rubbish, picking up out of place items etc.</p>
+                        <p>• All surfaces wiped down / disinfected</p>
+                        <p>• Disinfecting the bathroom and kitchen - sinks, mirrors, toilets, bathtub etc.</p>
+                        <p>• All mid-level surfaces dusted abd wiped clean</p>
+                        <p>• Wiping down all windowsills</p>
+                        <p>• Disinfect electronics and remotes</p>
+                    </div>
+                    <div className={styles.card}>
                         <h3>WHAT IS A DEEP CLEAN ?</h3>
                         <p>• Deep cleans are often required prior to a tenant moving in or out (end of tenancy clean) or if the customer has not previously invested in a cleaning service and the Home/Business needs more than a basic clean</p>
                         <br></br>
