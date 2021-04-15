57242e75162580e08c48e116816f161e328d263f
Tucker2015 <mail@kevtucker.com>
2021-04-15 12:19:34 +0100

    - Add Headers

--- pages/our_work.js
+++ pages/our_work.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Image from 'next/image'
+import Head from 'next/head'
 import { CarouselView } from '../components';
 import { SliderData } from '../components/Carousel/SliderData'
 import styles from '../styles/Home.module.css'
@@ -9,6 +10,16 @@ const OurWork = () => {
     return (
 
         <div className="container">
+            <Head>
+                <title>Global Cleaning Solutions</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="preload"
+                    href="/fonts/HWT-Gothic-Round.ttf"
+                    as="font"
+                    crossOrigin=""
+                />
+            </Head>
             <h1 className={styles.title}>
                 Our Work
         </h1>
