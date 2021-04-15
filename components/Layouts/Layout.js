91fd757f9f828843fa5c0908de464ccc2f018056
Tucker2015 <mail@kevtucker.com>
2021-04-15 10:54:20 +0100

    - Replace bootstrap with React Bootstrap

--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import styles from '../../styles/Layout.module.css'
 import Link from 'next/link'
 import Head from 'next/head'
-
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, } from 'react-bootstrap'
 export default function Layout({ children }) {
 
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
@@ -10,6 +10,24 @@ export default function Layout({ children }) {
 
     return (
         <>
+            <style type="text/css">
+                {`
+            .navBG {
+                background-color: black;
+                padding: 20px;
+            }
+            .navBrand {
+                color: #7aa850 !important;
+                font-size: 1.8em;
+                font-family: "GothicRound";
+            }
+            @media (max-width: 785px) {
+                .navBrand {
+                    font-size: 1.4em;
+                } 
+            }
+            `}
+            </style>
             <Head>
                 <title>Global Cleaning Solutions</title>
                 <link rel="icon" href="/favicon.ico" />
@@ -20,25 +38,35 @@ export default function Layout({ children }) {
                     crossOrigin=""
                 />
             </Head>
-            <header className={styles.header}>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top p-3">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#">Global Cleaning Solutions</a>
-                        <button className="custom-toggler navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
-                            <span className="navbar-toggler-icon" />
-                        </button>
-                        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
-                            <a className="nav-link text-info" href="/">Home</a>
-                            <a className="nav-link text-info" href="/contact">Contact</a>
-                            <a className="nav-link text-info" href="/services">Services</a>
-                            <a className="nav-link text-info" href="/our_work">Our Work</a>
-                            <a href="/request-demo" className="btn btn-sm btn-info nav-link text-white" >Request demo</a>
-                        </div>
-                    </div>
-                </nav>
-            </header>
+
+            <Navbar className="navBG" variant="dark" expand="lg">
+                <Navbar.Brand className="navBrand" href="#home">Global Cleaning Solutions</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Link href="/" passHref>
+                            <Nav.Link >Home</Nav.Link>
+                        </Link>
+                        <Link href="/services" passHref>
+                            <Nav.Link >Services</Nav.Link>
+                        </Link>
+                        <Link href="/our_work" passHref>
+                            <Nav.Link>Our Work</Nav.Link>
+                        </Link>
+                        <Link href="/about" passHref>
+                            <Nav.Link >About Us</Nav.Link>
+                        </Link>
+                        <Link href="/contact">
+                            <Nav.Link >Contact</Nav.Link>
+                        </Link>
+
+
+                    </Nav>
+                </Navbar.Collapse>
+
+
+            </Navbar>
+
             <main>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
                 {children}
--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* <link
-                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                        rel="stylesheet"
-                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                        crossOrigin="anonymous"
-                    /> */}
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossOrigin="anonymous"></script>
                 </Head>
                 <body>
